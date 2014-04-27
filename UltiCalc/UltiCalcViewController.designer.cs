b638ca0e75d1bd06b139194d9fdb26b3d9542664
theQure <rasmus_kure@hotmail.com>
2014-04-27 14:17:49 +0200

    AC

--- UltiCalc/UltiCalcViewController.designer.cs
+++ UltiCalc/UltiCalcViewController.designer.cs
@@ -1,15 +1,18 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
 
 namespace UltiCalc
 {
 	[Register ("UltiCalcViewController")]
 	partial class UltiCalcViewController
 	{
+		
 		void ReleaseDesignerOutlets ()
 		{
 		}
